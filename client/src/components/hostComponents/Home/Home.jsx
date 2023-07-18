f50c130129fe0d3c39cb7aced09af3287e75e296
Jamshadj <jamshadalij2004@gmail.com>
2023-07-18 09:32:46 +0530

    host navbar completed

--- client/src/components/hostComponents/Home/Home.jsx
+++ client/src/components/hostComponents/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import HostNavbar from '../HostNavbar/HostNavbar'
+import { useSelector } from 'react-redux';
 
 function Home() {
+  const {host } = useSelector((state) => state);
   return (
     <div>
       <HostNavbar/>
-        <h2>Home</h2>
+      <div className='inline-flex'>
+        <h2 className='pl-36 pt-12'>Welcome {host.details.firstName}!</h2>
+        <button>Complete Your Listing</button>
+        </div>
     </div>
   )
 }
