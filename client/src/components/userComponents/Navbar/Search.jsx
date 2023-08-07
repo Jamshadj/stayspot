72e8476f7b59641c0251bb22728f7db6afcc3f47
Jamshadj <jamshadalij2004@gmail.com>
2023-08-08 00:19:18 +0530

    location

--- client/src/components/userComponents/Navbar/Search.jsx
+++ client/src/components/userComponents/Navbar/Search.jsx
@@ -9,16 +9,12 @@ function Search() {
   const [checkInDate, setCheckInDate] = useState('');
   const [checkOutDate, setCheckOutDate] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
   const navigate = useNavigate();
-
   const handleSearchClick = () => {
     setShowInput(true);
   };
-
-  const handleLocationChange = (e) => {
-    setLocation(e.target.value);
-  };
-
   const fetchSuggestions = async (value) => {
     try {
       const response = await axios.get(
@@ -34,27 +30,12 @@ function Search() {
     }
   };
 
-  const handleSuggestionClick = (suggestion) => {
-    console.log("jejeeeeee");
-    const latitude = suggestion.coordinates[1];
-    const longitude = suggestion.coordinates[0];
-    const queryString = `?latitude=${latitude}&longitude=${longitude}`;
-    // navigate(`/matchingListing${queryString}`);
-    setSuggestions([]);
-  };
-
   const handleSearchChange = (e) => {
-    console.log("ded");
     const { value } = e.target;
-
     setLocation(value);
     fetchSuggestions(value);
   };
 
-  const handleInputBlur = () => {
-    setShowInput(false);
-    setLocation('');
-  };
 
   const handleCheckInDateChange = (e) => {
     setCheckInDate(e.target.value);
@@ -70,43 +51,33 @@ function Search() {
     return currentDate < now;
   };
 
-  // Function to disable dates before the check-in date
+  //   // Function to disable dates before the check-in date
   const disableBeforeCheckIn = (currentDate) => {
     const checkInDateObject = new Date(checkInDate);
     return currentDate <= checkInDateObject;
   };
 
-  const hell=()=>{
-    console.log("ggg");
-  }
 
-  const searchListingsByLocation = async () => {
+  const searchListingsByLocation = () => {
     try {
-      console.log("wdwed");
-      const response = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json`,
-        {
-          params: {
-            access_token: 'pk.eyJ1IjoiamFtc2hhZDEiLCJhIjoiY2xrOXc0cXM1MDFkYjNtcWQ3NDVmZmh4ciJ9.GCP7IIfzt1ms84ZeOr7uag',
-          },
-        }
-      );
-    console.log(response);
-      const coordinates = response.data.features[0].geometry.coordinates;
-      const latitude = coordinates[1];
-      const longitude = coordinates[0];
-     
-      const queryString = `?latitude=${latitude}&longitude=${longitude}`;
+      const queryString = `?latitude=${latitude}&longitude=${longitude}&checkInDate=${checkInDate}&checkInOut=${checkOutDate}`;
       navigate(`/matchingListing${queryString}`);
     } catch (error) {
-      console.error('Error fetching matching listings:', error);
-    }
-  };
 
+    }
+  }
+
+  const handle = (location) => {
+    setLocation(location.location)
+    setLatitude(location.coordinates[1])
+    setLongitude(location.coordinates[0])
+    setSuggestions([])
+    console.log("hello", location);
+  }
   return (
     <div
       className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
-      onClick={handleSearchClick}
+
     >
       {showInput ? (
         <div className="flex flex-row items-center">
@@ -116,9 +87,8 @@ function Search() {
             placeholder="Location"
             value={location}
             onChange={handleSearchChange}
-            onBlur={handleInputBlur}
           />
-           <label className="px-4 py-1 text-sm text-gray-600">Check-in</label>
+          <label className="px-4 py-1 text-sm text-gray-600">Check-in</label>
           <input
             type="date"
             className="w-[22%] px-2 py-1 text-sm outline-none"
@@ -139,11 +109,11 @@ function Search() {
             className="p-2 bg-rose-500 rounded-full text-black"
             onClick={searchListingsByLocation}
           >
-            <FaSearch onClick={hell} size={18} />
+            <FaSearch size={18} />
           </div>
         </div>
       ) : (
-        <div className="flex flex-row items-center justify-between">
+        <div onClick={handleSearchClick} className="flex flex-row items-center justify-between">
           <div className="text-sm font-semibold px-6">Anywhere</div>
           <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
             Anywhere
@@ -157,7 +127,7 @@ function Search() {
         </div>
       )}
       {/* Suggestions dropdown */}
-      {showInput && suggestions.length > 0 && (
+      {suggestions.length > 0 && (
         <ul className="absolute z-10 w-full bg-white border border-gray-300 rounded-md shadow-lg" style={{
           marginLeft: "21px", width: "29rem"
         }}>
@@ -165,13 +135,14 @@ function Search() {
             <li
               key={suggestion.location}
               className="px-3 py-2 hover:bg-gray-100 cursor-pointer"
-              onClick={() => handleSuggestionClick(suggestion)}
+              onClick={() => handle(suggestion)}
             >
               {suggestion.location}
             </li>
           ))}
         </ul>
       )}
+
     </div>
   );
 }
