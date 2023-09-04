2704185bd15c57bfcf0cd64b71dbfe81092ae9cd
Jamshadj <jamshadalij2004@gmail.com>
2023-09-04 17:21:38 +0530

    url changed

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async function dbConnect() {
   try {
     mongoose.set('strictQuery', false);
-    await mongoose.connect("mongodb://mongo_db:27017/stayspot"); // Use the service name as the hostname
+    await mongoose.connect("mongodb://:127.0.0.1/stayspot"); // Use the service name as the hostname
     console.log("db connected");
   } catch (error) {
     console.error("db connection error:", error);
