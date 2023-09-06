780676101a925342bc815b64e503cc44be03cc02
Jamshadj <jamshadalij2004@gmail.com>
2023-09-06 19:10:18 +0530

    completed

--- client/src/constant/localStorage.js
+++
@@ -1,8 +0,0 @@
-export const setItem = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value));
-  };
-
-  export const getItem = (key) => {
-    const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : null;
-  };
