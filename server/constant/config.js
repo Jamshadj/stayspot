a8e4bea2003186858ca3affe9a207dbe85c1afef
Jamshadj <jamshadalij2004@gmail.com>
2023-09-28 18:55:51 +0530

    cloudinary config for upload image in cloud

--- server/constant/config.js
+++ server/constant/config.js
@@ -7,6 +7,5 @@ cloudinary.v2.config({
   api_key:process.env.CLOUDINARY_API_KEY,
   api_secret:process.env.CLOUDINARY_API_SECRET
 
-})
-                                      
+})     
 export default cloudinary.v2
