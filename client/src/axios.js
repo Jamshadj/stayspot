e934720b4ca3f9a22d22be72f08281191767b115
Jamshadj <jamshadalij2004@gmail.com>
2024-06-06 17:31:45 +0530

    build updated

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,7 +3,7 @@ import { BASE_URL } from './constant/constant';
 
 const axiosInstance = (tokenName) => {
   const instance = axios.create({
-    baseURL: "http://3.25.135.114/api",
+    baseURL: "https://stayspot.jamshad.online/api",
     withCredentials: true,
     headers: {
       'Content-Type': 'application/json'
