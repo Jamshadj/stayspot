5687c7547e79146e09a98fae75195ee31315ea34
Jamshadj <jamshadalij2004@gmail.com>
2024-06-06 14:21:54 +0530

    build file updated

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,7 +3,7 @@ import { BASE_URL } from './constant/constant';
 
 const axiosInstance = (tokenName) => {
   const instance = axios.create({
-    baseURL: "http://54.206.52.169",
+    baseURL: "http://3.25.135.114",
     withCredentials: true,
     headers: {
       'Content-Type': 'application/json'
